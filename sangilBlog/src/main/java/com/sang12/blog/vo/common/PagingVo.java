7e10868412698ac8ea0fa1fd5ce8ad74693b7455
ChoiSangIl <wo566@naver.com>
2018-07-31 21:35:33 +0900

    mybatis paging vo 설계.. 집에서 작업하기 위해 커밋

--- sangilBlog/src/main/java/com/sang12/blog/vo/common/PagingVo.java
+++ sangilBlog/src/main/java/com/sang12/blog/vo/common/PagingVo.java
@@ -1,5 +1,9 @@
 package com.sang12.blog.vo.common;
 
+import java.io.Serializable;
+
+import lombok.Data;
+
 /**
  * com.sang12.blog.vo.common
  * PagingVo.java
@@ -8,6 +12,55 @@ package com.sang12.blog.vo.common;
  * @Date 2018. 7. 30.
  * @Description : 공통 paging vo
  */
-public class PagingVo {
+@Data
+public class PagingVo implements Serializable {
 
+	private static final long serialVersionUID = -8380031519896341326L;
+	
+	//db에서 페이징 할 시작번호
+	private int start;
+	
+	//db에서 페이징 할 종료번호
+	private int end;
+	
+	//현재페이지
+	private int page;
+	
+	//하단에 ㅇ표시 할 페이징 범위
+	private int countPage;
+	
+	//출력될 게시 물 수 
+	private int countList;
+	
+	//총 게시물 수
+	private int totalCount;
+	
+	//전체 페이지 수
+	private int totalPage;
+	
+	//하단에 표시할 페이지 시작 번호
+	private int startPage;
+	
+	//하단에 표시할 페이지 마지막 번호 
+	private int endPage;
+	
+	public void setPaging() {
+		totalPage = totalCount / countList;
+
+		if (totalCount % countList > 0) {
+		    totalPage++;
+		}
+		
+		if (totalPage < page) {
+		    page = totalPage;
+		}
+		
+		startPage = ((page - 1) / 10) * 10 + 1;
+		endPage = startPage + countPage - 1;
+		
+		if (endPage > totalPage) {
+		    endPage = totalPage;
+		}
+	}
+	
 }
